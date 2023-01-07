c5a76c89e2204ff1071bff2f9bd5cbc64e703905
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2023-01-07 03:12:43 -0300

    Demo

--- client/src/components/Paginado.jsx
+++ client/src/components/Paginado.jsx
@@ -12,26 +12,25 @@ const paginado = ({paginaAnterior,paginaSiguiente,dataSearch,data,currentElement
     padding:'4px',
     background:'#2c2c2c81',
     color:'#fff',
-    cursor:'not-allowed'
+    cursor:'default'
   }
   let datos = data && data.map(e=>e.nombre)
   let elementosPorPagina = currentElements && currentElements.map(e=>e.nombre)
 
-  let datosSearch = dataSearch && dataSearch.map(e=>e.nombre)
-  let elementosSearch = currentElementsSearch && currentElementsSearch.map(e=>e.nombre)
+  let datosSearch = dataSearch.length && dataSearch.map(e=>e.nombre)
+  let elementosSearch = currentElementsSearch.length  && currentElementsSearch.map(e=>e.nombre)
 
   
   return (
     // VALIDAR CUANDO EL ARRAY del search ESTA VACIO PARA DESABILITAR LOS BOTONES
     <div>
       <ul>
-        {
+        { 
           datos[0] === elementosPorPagina[0] ? <a style={disable} href='#'>Anterior</a> : <a style={margen} href='#' onClick={()=>paginaAnterior()}>Anterior</a>
         }
         {
           datos[data.length-1] === elementosPorPagina[currentElements.length-1] ? <a style={disable} href='#'>Siguiente</a> : <a style={margen} href='#' onClick={()=>paginaSiguiente()}>Siguiente</a>
         }
-        
       </ul>
     </div>
   )
